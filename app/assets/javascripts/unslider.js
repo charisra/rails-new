0c654e241ce990f97a599335ee31f2805a037009
codecademy <ccuser@codecademy.com>
2016-05-09 17:25:32 -0400

    1

---
+++ app/assets/javascripts/unslider.js
@@ -0,0 +1,3 @@
+<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="/unslider.js"></script> <!-- but with the right path! -->
+<script>$(function() { $('.banner').unslider() })</script>
